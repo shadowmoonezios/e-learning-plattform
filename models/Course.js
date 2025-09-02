3d10f23b4246d794bb3cf16e21590181b20840c4
shadowmoonezios <agushifelde@hotmail.com>
2025-09-02 19:25:03 +0300

    feat: initial commit for models/Course.js

---
+++ models/Course.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    content: { type: String },
+    instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Course', courseSchema);
